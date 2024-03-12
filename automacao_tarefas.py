46ca1e7fe0a2de97d1aefb445afc93c6a99ef168
Débora  Santos <deboramargaretedossantos@gmail.com>
2024-03-12 14:12:36 -0300

    automacao_cadastro_final

--- automacao_tarefas.py
+++ automacao_tarefas.py
@@ -1,27 +1,78 @@
-import pyautogui
+import pyautogui            
 import time
-import pandas as pd
 
 pyautogui.PAUSE = 0.5
 
-pyautogui.press ('win')
+pyautogui.press ('win') 
 pyautogui.write ('chrome')
-pyautogui.press ('enter')
+pyautogui.press ('enter') 
 
-link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
-pyautogui.write (link)
-pyautogui.press ('enter')
 
-time.sleep (1)
+link = ('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.write(str(link))
+pyautogui.press('enter') 
+time.sleep(3)
 
-pyautogui.click (x=513, y=372)
+pyautogui.click(x=569, y=368)
 pyautogui.write('deborasantos@gmail.com')
+pyautogui.press('enter')
+pyautogui.click(x=467, y=471)
 
-pyautogui.click (x=559, y=465)
 pyautogui.write('****')
-pyautogui.press ('enter')
+pyautogui.press('enter')
 
-time.sleep (3)
+import pandas as pd
+tabela = pd.read_csv("produtos.csv")
+print(tabela)
+
+for linha in tabela.index:
+    pyautogui.click(x=597, y=261)
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(str(codigo))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
